bf48932266654c50d6ce2cfcaaaf11892d4c996f
bgallagher-9 <b.gallagher9@gmail.com>
2017-05-03 09:09:10 -0500

    express-zipcodes

---
+++ views/index.js
@@ -0,0 +1,16 @@
+<% include header %>
+
+<h1>What's your ZIP code?</h1>
+
+<form class="pure-form">
+  <fieldset>
+    <input type="number" name="zip" placeholder="12345" autofocus required />
+    <input type="submit" class="pure-button pure-button-primary" value="Go" />
+  </fieldset>
+</form>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<script>"/main.js"</script>
+
+<% include footer %>
